9f857094a2411565d92fee20ead354322d80a8cf
echooffx <echooff125@outlook.com>
2026-04-29 14:46:22 +0800

    test cicd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +6,12 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello CI/CD, FastAPI is running!"}
+    # 模拟读取环境变量，如果没有配置，则使用默认值
+    app_env = os.getenv("APP_ENV", "development")
+    db_host = os.getenv("DB_HOST", "localhost (default)")
+
+    return {
+        "message": "MVP Application is running!",
+        "environment": app_env,
+        "database_host": db_host,
+    }
---
+++ test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 500
+    assert "MVP Application is running!" in response.json()["message"]
